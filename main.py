f9bad7125362ebbe6312381c88ef7a6536d87db5
Beverly <bq2159@columbia.edu>
2025-12-11 15:06:38 -0500

    Dockerized microservice for Cloud Run

--- main.py
+++ main.py
@@ -22,10 +22,12 @@ async def health_check():
 
 
 if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8004,
+        port=port,
         reload=True
     )
 
